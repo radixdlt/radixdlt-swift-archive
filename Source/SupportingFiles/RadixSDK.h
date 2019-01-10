ba414a1452a88480aa2ef21eba3243c60054414b
Alexander Cyon <alex@radixdlt.com>
2019-01-10 14:23:03 +0000

    Renaming project RadixSDK, renaming Info.plists to contain the Target name. Adding 'SupportingFiles' folder for Source and Tests for Info.plist files and misc files.

---
+++ Source/SupportingFiles/RadixSDK.h
@@ -0,0 +1,19 @@
+//
+//  RadixSDK.h
+//  RadixSDK
+//
+//  Created by Alexander Cyon on 2019-01-10.
+//  Copyright © 2019 Radix DLT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RadixSDK.
+FOUNDATION_EXPORT double RadixSDKVersionNumber;
+
+//! Project version string for RadixSDK.
+FOUNDATION_EXPORT const unsigned char RadixSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RadixSDK/PublicHeader.h>
+
+
--- Tests/TestCases/Crypto/Keys/PrivateKeyTests.swift
+++ Tests/TestCases/Crypto/Keys/PrivateKeyTests.swift
@@ -1,15 +1,15 @@
 //
-//  RadixDLTSDKTests.swift
-//  RadixDLTSDKTests
+//  PrivateKeyTests.swift
+//  RadixSDKTests
 //
 //  Created by Alexander Cyon on 2019-01-10.
 //  Copyright © 2019 Radix DLT. All rights reserved.
 //
 
 import XCTest
-@testable import RadixDLTSDK
+@testable import RadixSDK
 
-class RadixDLTSDKTests: XCTestCase {
+class PrivateKeyTests: XCTestCase {
 
     func testTrivial() {
         XCTAssert(true)
