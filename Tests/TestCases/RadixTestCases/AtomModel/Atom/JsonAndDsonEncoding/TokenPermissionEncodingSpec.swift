6359215389f0692e6499fb016220bec654cdd51c
Alexander Cyon <alex@radixdlt.com>
2019-03-13 13:57:24 +0100

    Adding JSON and DSON encoding tests of TokenPermissions to ensure correctness in encoding, i.e. for values JSON encoded as strings, such as TokenPermission they contain the JSON prefix: `:str:` but this prefix should not be included in the DSON encoding

---
+++ Tests/TestCases/RadixTestCases/AtomModel/Atom/JsonAndDsonEncoding/TokenPermissionEncodingSpec.swift
@@ -0,0 +1,39 @@
+//
+//  TokenPermissionEncoding.swift
+//  RadixSDK iOS Tests
+//
+//  Created by Alexander Cyon on 2019-03-13.
+//  Copyright © 2019 Radix DLT. All rights reserved.
+//
+
+import Foundation
+@testable import RadixSDK
+import Quick
+import Nimble
+
+class TokenPermissionEncodingSpec: QuickSpec {
+    override func spec() {
+        describe("Encoding of TokenPermission") {
+            let permissions = TokenPermissions.all
+            describe("to JSON") {
+                let jsonString = String(
+                    data: try! JSONEncoder().encode(permissions),
+                    encoding: .utf8
+                )!
+                it("its values are prefixed with :str:") {
+                    expect(jsonString).to(contain(":str:all"))
+                }
+            }
+            describe("to Dson") {
+                let dsonHex = permissions.toDSON().hex
+                it("its values are not prefixed with :str:") {
+                    expect(dsonHex).to(contain("burn".toDSON().hex + "all".toDSON().hex))
+                    expect(dsonHex).to(contain(
+                        ["burn", "all"].map { $0.toDSON().hex }.joined()
+                    ))
+                    expect(dsonHex).toNot(contain(":str:all".toDSON().hex))
+                }
+            }
+        }
+    }
+}
