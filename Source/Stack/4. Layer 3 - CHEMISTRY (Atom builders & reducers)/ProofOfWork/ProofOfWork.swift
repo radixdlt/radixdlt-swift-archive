ebb5929f35b6679d6f558582be9bbbe274d7e5ab
Alexander Cyon <alex@radixdlt.com>
2019-04-18 11:29:00 +0200

    Moving ProofOfWorkWorker out to separate file

--- Source/Stack/4. Layer 3 - CHEMISTRY (Atom builders & reducers)/ProofOfWork/ProofOfWork.swift
+++ Source/Stack/4. Layer 3 - CHEMISTRY (Atom builders & reducers)/ProofOfWork/ProofOfWork.swift
@@ -15,11 +15,22 @@ public struct ProofOfWork: CustomStringConvertible {
     private let magic: Magic
     private let nonce: Nonce
     
-    fileprivate init(seed: Data, targetHex: HexString, magic: Magic, nonce: Nonce) {
+    internal init(seed: Data, targetHex: HexString, magic: Magic, nonce: Nonce) {
         self.seed = seed
         self.targetHex = targetHex
         self.magic = magic
         self.nonce = nonce
+    }
+}
+
+// MARK: - Prove
+public extension ProofOfWork {
+    func prove() throws {
+        let unhashed: Data = magic.magicTo4BigEndianBytes + seed + nonce.as8BytesBigEndian
+        let hashHex = RadixHash(unhashedData: unhashed).hex
+        guard hashHex <= targetHex.hex else {
+            throw Error.expected(hex: hashHex, toBeLessThanOrEqualToTargetHex: targetHex.hex)
+        }
     }
 }
 
@@ -37,105 +48,6 @@ public extension ProofOfWork {
     }
 }
 
-import RxSwift
-public final class ProofOfWorkWorker {
-    private let dispatchQueue = DispatchQueue(label: "pow", qos: .background)
-    public init() {}
-    
-    static let expectedByteCountOfSeed = 32
-    
-    func work(
-        atom: Atom,
-        magic: Magic,
-        numberOfLeadingZeros: ProofOfWork.NumberOfLeadingZeros = .default
-        ) -> Observable<ProofOfWork> {
-     
-        return work(
-            seed: atom.radixHash.asData,
-            magic: magic,
-            numberOfLeadingZeros: numberOfLeadingZeros
-        )
-    }
-    
-    func work(
-        seed: Data,
-        magic: Magic,
-        numberOfLeadingZeros: ProofOfWork.NumberOfLeadingZeros = .default
-    ) -> Observable<ProofOfWork> {
-        return Observable.deferred {
-            return Observable.create { observer in
-                
-                let calculatePOW = DispatchWorkItem { [weak self] in
-                    guard let self = self else { return }
-                    return self.work(
-                        seed: seed,
-                        magic: magic,
-                        numberOfLeadingZeros: numberOfLeadingZeros
-                    ) { resultOfWork in
-                        switch resultOfWork {
-                        case .failure(let error):
-                            observer.onError(error)
-                        case .success(let pow):
-                            observer.onNext(pow)
-                            observer.onCompleted()
-                        }
-                    }
-                }
-                self.dispatchQueue.async(execute: calculatePOW)
-                
-                return Disposables.create {
-                    calculatePOW.cancel()
-                }
-            }
-        }
-    }
-
-    private func work(
-        seed: Data,
-        magic: Magic,
-        numberOfLeadingZeros: ProofOfWork.NumberOfLeadingZeros = .default,
-        done: ((Result<ProofOfWork, Error>) -> Void)
-    ) {
-        guard seed.length == ProofOfWorkWorker.expectedByteCountOfSeed else {
-            let error = ProofOfWork.Error.workInputIncorrectLengthOfSeed(expectedByteCountOf: ProofOfWorkWorker.expectedByteCountOfSeed, butGot: seed.length)
-            done(.failure(error))
-            return
-        }
-        
-        let numberOfBits = ProofOfWorkWorker.expectedByteCountOfSeed * Int.bitsPerByte
-        var bitArray = BitArray(repeating: .one, count: numberOfBits)
-        
-        for index in 0..<Int(numberOfLeadingZeros.numberOfLeadingZeros) {
-            bitArray[index] = .zero
-        }
-        
-        let target = bitArray.asData
-        let targetHex = bitArray.hex
-        var nonce: Nonce = 0
-        let base: Data = magicTo4BigEndianBytes(magic) + seed
-        var hex: String
-        repeat {
-            nonce += 1
-            let unhashed = base + nonce.as8BytesBigEndian
-            hex = RadixHash(unhashedData: unhashed).hex
-        } while hex > targetHex
-        
-        let pow = ProofOfWork(seed: seed, targetHex: target.toHexString(), magic: magic, nonce: nonce)
-        done(.success(pow))
-    }
-    
-}
-
-public extension ProofOfWork {
-    
-    func prove() throws {
-        let unhashed: Data = magicTo4BigEndianBytes(magic) + seed + nonce.as8BytesBigEndian
-        let hashHex = RadixHash(unhashedData: unhashed).hex
-        guard hashHex <= targetHex.hex else {
-            throw Error.expected(hex: hashHex, toBeLessThanOrEqualToTargetHex: targetHex.hex)
-        }
-    }
-}
 
 // MARK: - Error
 public extension ProofOfWork {
@@ -145,19 +57,20 @@ public extension ProofOfWork {
     }
 }
 
-// MARK: - Endianess (Matching Java library ByteStream `putInt`)
-private func magicTo4BigEndianBytes(_ magic: Magic) -> [Byte] {
-    let magic4Bytes = CFSwapInt32HostToBig(UInt32(magic)).bytes
-    assert(magic4Bytes.count == 4)
-    return magic4Bytes
+internal extension Magic {
+    // MARK: - Endianess (Matching Java library ByteStream `putInt`)
+    var magicTo4BigEndianBytes: [Byte] {
+        let magic4Bytes = CFSwapInt32HostToBig(UInt32(self)).bytes
+        assert(magic4Bytes.count == 4)
+        return magic4Bytes
+    }
 }
 
 // MARK: - Endianess (Matching Java library ByteStream `putLong`)
-private extension Nonce {
+internal extension Nonce {
     var as8BytesBigEndian: [Byte] {
         let nonce8Bytes = CFSwapInt64HostToBig(UInt64(value)).bytes
         assert(nonce8Bytes.count == 8)
         return nonce8Bytes
     }
 }
-
